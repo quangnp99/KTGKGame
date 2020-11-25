093390486fdf1d3d67bf2398b9b2ed3a432c70d1
Quang <phuongquangnguyen99@gmail.com>
2020-11-25 16:17:27 +0700

    commit

---
+++ Assets/GemCount.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemCount : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Score.playerScore += 1;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int playerScore;
+    Text scoretext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoretext = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoretext.text = playerScore.ToString();
+    }
+}
